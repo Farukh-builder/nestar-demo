6b73e52035afc03545cb038e9b9c45459f659cc8
Farukh-builder <farukhtukhtamishev1@gmail.com>
2025-10-13 16:32:49 +0900

    feat: organize and test routing system

--- pages/index.tsx
+++ pages/index.tsx
@@ -48,13 +48,8 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image
-                className={styles.logo}
-                src="/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
-              />
+            
+              <h1> PAGES ROUTER</h1>
               Deploy now
             </a>
             <a
---
+++ pages/property/index.tsx
@@ -0,0 +1,5 @@
+const PropertyLIst = () => {
+    return <div>PROPERTY LIST</div>
+}
+
+export default PropertyLIst;

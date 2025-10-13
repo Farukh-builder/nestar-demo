91f27d19c1d5153d525904500d27bc7fb408231a
Farukh-builder <farukhtukhtamishev1@gmail.com>
2025-10-13 20:46:11 +0900

    feat: integration Header and Footer via layouts

--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -1,5 +1,13 @@
-const PropertyLIst = () => {
-    return <div>PROPERTY LIST</div>
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { WifiTethering } from "@mui/icons-material";
+import { Container, Stack } from "@mui/material";
+import { NextPage } from "next";
+
+
+const PropertyLIst: NextPage = () => {
+    return (
+    <Container>PROPERTY LIST</Container>
+    )
 }
 
-export default PropertyLIst;
+export default withLayoutBasic(PropertyLIst);
